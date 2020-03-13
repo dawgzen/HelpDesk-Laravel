075bb23e7da6e14d67d279ee574d471f1a7cf641
dawgzen <dennisverhaagen@hotmail.com>
2020-03-13 14:19:45 +0100

    initial commit + stap1

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Support\Facades\DB;
+
+class TestController extends Controller
+{
+
+
+    public function show($id)
+    {
+        return view('test' ,['id' => $id]);
+    }
+    public function index()
+    {
+        $klanten = DB::select('select * from klanten', [1]);
+
+        return view('test', ['klanten' => $klanten]);
+    }
+}
