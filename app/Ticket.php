96f8d1ed347b32d1f8bf49bcc684e192c4dd38d8
dennis verhaagen <dennisverhaagen@hotmail.com>
2020-03-24 16:32:29 +0100

    stap 8 complete

---
+++ app/Ticket.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Ticket extends Model
+{
+    protected $guarded = [];
+
+    public function submitting_user()
+    {
+        return $this->belongsTo('App\User', "user_id");
+    }
+    public function assigned_user(){
+        return $this->belongsToMany("App\User");
+    }
+    public function status()
+    {
+        return $this->belongsTo('App\Status')->withDefault();
+    }
+    public function comments(){
+        return $this->hasMany("App\Comment");
+    }
+}
