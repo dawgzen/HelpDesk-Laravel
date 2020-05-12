d8db0326b847e05388168813f496ee6a917a7693
dennis verhaagen <dennisverhaagen@hotmail.com>
2020-05-12 15:03:20 +0200

    step 28 done

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('404 not found') }}</div>
+                    <div class="card-body">
+                        {{__('It seems like this page doesnt exist friend, please go back and try again. or dont, i dont really care.')}}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
